aa0794a4359c0f73746278262e9824fd73e44abc
Etienne Leclerc <ewall1717@gmail.com>
2022-11-28 16:42:39 -0500

    fix main plots

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ T_w = 100+273.15     # [K] Température de base
 R = .2       # [m] Rayon 
 h = 1      # [W/m^2*K] Coefficient de convection
 Bi = 1
-nr = 20      # [-] Nombre de points en z
+nr = 20     # [-] Nombre de points en z  "prendre plus que 20 n'est pas necessaire selon erreur avec analytique"
 nz = 20
 CL = "isole"
     
@@ -69,10 +69,13 @@ for Bi_i in list_Bi:
 
 #=========================2e analyse - Base=========================
 list_Bi = np.linspace(.01,100,100)
-#list_Bi = [0.1, 1,100]
 r = np.linspace(prm.R, 0, prm.nr)
+z = np.linspace(0, prm.L, prm.nz)
 q_base_isole = []
 q_base_convection = []
+q_contour_isole = []
+q_contour_convection_bout = []
+q_contour_convection_sansbout = []
 for i in range(len(list_Bi)):
     prm.setBi(list_Bi[i])
     prm.setCL("isole")
@@ -87,19 +90,43 @@ for i in range(len(list_Bi)):
     c_reshaped = c.reshape(prm.nz,prm.nr).transpose()
     T = c_reshaped
     q_base_convection.append(inte_fluxBase(T,r,prm))
+    bout = True
+    D=2
     
-plt.title("Q en fonction du nombre de Bi et flux base")    
-plt.plot(list_Bi,q_base_isole,label="cond isole")  
-plt.plot(list_Bi,q_base_convection,label="cond convection")  
+    prm.setBi(list_Bi[i])
+    prm.setCL("isole")
+    A,b = mdf_assemblage(X,Y,prm)
+    c = np.linalg.solve(A,b) 
+    c_reshaped = c.reshape(prm.nz,prm.nr).transpose()
+    T= c_reshaped
+    q_contour_isole.append(inte_fluxContour(T,z,r,bout,D,prm))
+    prm.setCL("convection")
+    A,b = mdf_assemblage(X,Y,prm)
+    c = np.linalg.solve(A,b) 
+    c_reshaped = c.reshape(prm.nz,prm.nr).transpose()
+    T = c_reshaped
+    q_contour_convection_bout.append(inte_fluxContour(T,z,r,bout,D,prm))
+    bout = False
+    q_contour_convection_sansbout.append(inte_fluxContour(T,z,r,bout,D,prm))
+    
+plt.title("Q en fonction du nombre de Bi")    
+plt.plot(list_Bi,q_base_isole,label="cond isole - flux base")  
+plt.plot(list_Bi,q_base_convection,label="cond convection - flux base")  
+plt.plot(list_Bi,q_contour_isole,label="cond isole - flux contour")  
+plt.plot(list_Bi,q_contour_convection_sansbout,label="cond convection - flux contour") 
+plt.plot(list_Bi,q_contour_convection_bout,'--', color="orange",label="cond convection + bout - flux contour") 
+plt.ylabel("q (W)")
+plt.xlabel("Bi") 
 plt.legend()
-plt.savefig("q_fluxBase.png", dpi=400)
+plt.savefig("q_flux.png", dpi=400)
 plt.show()
 #=========================2e analyse - Contour=========================
 list_Bi = np.linspace(.01,100,100)
-#list_Bi = [0.1, 1,100]
 q_contour_isole = []
-q_contour_convection = []
+q_contour_convection_bout = []
+q_contour_convection_sansbout = []
 q_analytique=[]
+r = np.linspace(prm.R, 0, prm.nr)
 z = np.linspace(0, prm.L, prm.nz)
 for Bi_i in list_Bi:
     bout = True
@@ -117,7 +144,9 @@ for Bi_i in list_Bi:
     c = np.linalg.solve(A,b) 
     c_reshaped = c.reshape(prm.nz,prm.nr).transpose()
     T = c_reshaped
-    q_contour_convection.append(inte_fluxContour(T,z,r,bout,D,prm))
+    q_contour_convection_bout.append(inte_fluxContour(T,z,r,bout,D,prm))
+    bout = False
+    q_contour_convection_sansbout.append(inte_fluxContour(T,z,r,bout,D,prm))
     D=1
     bout = False
     T = ref_analytique(z,prm)
@@ -125,26 +154,28 @@ for Bi_i in list_Bi:
 
 plt.title("Q en fonction du nombre de Bi et flux contour")    
 plt.plot(list_Bi,q_contour_isole,label="cond isole")  
-plt.plot(list_Bi,q_contour_convection,'--', color="orange",label="cond convection + bout")  
+plt.plot(list_Bi,q_contour_convection_bout,'--', color="orange",label="cond convection + bout")  
+plt.plot(list_Bi,q_contour_convection_sansbout,label="cond convection")  
 plt.plot(list_Bi,q_analytique,'--r',label="analytique")  
+plt.ylabel("q (W)")
+plt.xlabel("Bi")
 plt.legend()
 plt.savefig("q_fluxContour.png", dpi=400)
 plt.show()
 # # =========================2e analyse - Erreur =========================
 erreur=[]
 for i in range(len(list_Bi)):
-    erreur.append(abs(q_analytique[i]-q_contour_isole[i]))
+    erreur.append(abs(q_analytique[i]-q_contour_isole[i])/q_analytique[i]*100)
 plt.title("Erreur entre analytique et isole flux contour")    
 plt.plot(list_Bi,erreur,label="erreur")  
+plt.ylabel("Pourcentage (%)")
+plt.xlabel("Bi")
 plt.legend()
 plt.savefig("q_erreur_fluxContour.png", dpi=400)
 plt.show()
 
-
-
 # # =========================Analyse Bonus=========================
 list_Bi = [0.1, 1, 10, 20, 100]
-#list_Bi = [0.1, 1]
 
 condition_limite = ["isole","convection"]
 for Bi_i in list_Bi:
@@ -160,5 +191,6 @@ for Bi_i in list_Bi:
         ax.set_title("Profil Bi="+str(prm.Bi)+"CL="+str(prm.CL))
         ax.set_xlabel("Position z")
         ax.set_ylabel("Position r")
+        ax.set_zlabel("Temperature (K)")
         plt.savefig("Profil2D_Bi"+str(prm.Bi)+"_"+str(prm.CL)+".png", dpi=400)
         plt.show()
